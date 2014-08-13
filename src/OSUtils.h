0824e0e0872e6aca0371c41c02dc51da73b891b5
pauldardeau <pauldardeau@me.com>
2014-08-13 13:26:56 -0500

    added crc32

--- src/OSUtils.h
+++ src/OSUtils.h
@@ -22,6 +22,13 @@ namespace chaudiere
       static std::string getCurrentDirectory();
       
       /**
+       * Determines if the specified directory exists
+       * @param directory the directory whose existence is to be tested
+       * @return boolean indicating if the directory exists
+       */
+      static bool directoryExists(const std::string& directory);
+      
+      /**
        * Joins (concatenates) a directory path and file name and will include the path delimiter if necessary
        * @param directory the path for the directory
        * @param fileName the name of the file
@@ -31,10 +38,29 @@ namespace chaudiere
       
       /**
        * Determines if the specified file path exists
-       * @param filePath the file path whose existence is to tested
+       * @param filePath the file path whose existence is to be tested
        * @return boolean indicating if the file path exists
        */
       static bool pathExists(const std::string& filePath);
+      
+      /**
+       *
+       * @param inCrc32
+       * @param buf
+       * @param bufLen
+       * @return
+       */
+      static unsigned long crc32ForBuffer(unsigned long inCrc32,
+                                          const void *buf,
+                                          size_t bufLen);
+
+      /**
+       * Computes the CRC-32 value for the specified file
+       * @param filePath the path to the file
+       * @param crc32 will be populated with the CRC-32 value as a hex string
+       * @return boolean indicating if the CRC value was obtained for the file
+       */
+      static bool crc32ForFile(const std::string& filePath, std::string& crc32);
 
    };
 
