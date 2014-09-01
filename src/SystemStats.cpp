a09f973bae99ecd6a9f590c154c2e6d9d98a8636
pauldardeau <pauldardeau@me.com>
2014-09-01 17:50:34 -0500

    implemented uptimeSeconds and getNumberProcesses

--- src/SystemStats.cpp
+++ src/SystemStats.cpp
@@ -2,6 +2,13 @@
 // BSD License
 
 #include <stdlib.h>
+#include <time.h>
+
+#ifdef __linux__
+#include <sys/sysinfo.h>
+#else
+#include <sys/sysctl.h>
+#endif
 
 #include "SystemStats.h"
 
@@ -11,8 +18,35 @@ using namespace chaudiere;
 
 bool SystemStats::uptimeSeconds(long long& uptimeSeconds) noexcept
 {
-   //TODO: implement uptimeSeconds
-   return false;
+   bool success = false;
+   
+#ifdef __linux__
+   struct sysinfo si;
+   memset(&si, 0, sizeof(si));
+   
+   const int rc = sysinfo();
+   
+   if (rc == 0) {
+      uptimeSeconds = si.uptime;  // seconds since boot
+      success = true;
+   }
+#else
+   struct timeval boottime;
+   size_t len = sizeof(boottime);
+   int mib[2] = { CTL_KERN, KERN_BOOTTIME };
+   if (::sysctl(mib, 2, &boottime, &len, NULL, 0) < 0) {
+      return false;
+   }
+   
+   time_t bsec = boottime.tv_sec;
+   time_t csec = time(NULL);
+
+   uptimeSeconds = difftime(csec, bsec);
+   
+   success = true;
+#endif
+          
+   return success;
 }
 
 //******************************************************************************
@@ -40,8 +74,45 @@ bool SystemStats::getLoadAverages(double& oneMinute,
 
 bool SystemStats::getNumberProcesses(int& numberProcesses) noexcept
 {
-   //TODO: implement getNumberProcesses
-   return false;
+   bool success = false;
+   
+#ifndef __linux__
+   int rc;
+   size_t length = 0;
+   static const int name[] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0 };
+   
+   // Call sysctl with a NULL buffer to get proper length
+   rc = ::sysctl((int *)name,
+                 (sizeof(name) / sizeof(*name)) - 1,
+                 NULL,
+                 &length,
+                 NULL,
+                 0);
+   
+   if (rc == 0) {
+      // Allocate buffer
+      struct kinfo_proc* proc_list =
+         (struct kinfo_proc*)::malloc(length);
+       
+      if (proc_list != nullptr) {
+         // Get the actual process list
+         rc = ::sysctl((int *)name,
+                       (sizeof(name) / sizeof(*name)) - 1,
+                       proc_list,
+                       &length,
+                       NULL,
+                       0);
+         if (rc == 0) {
+            numberProcesses = length / sizeof(struct kinfo_proc);
+            success = true;
+         }
+      } else {
+         ::free(proc_list);
+      }
+   }
+#endif
+      
+   return success;
 }
 
 //******************************************************************************
